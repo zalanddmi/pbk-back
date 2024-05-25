91a0c505b75733ef94c7c18e0b7bec36dcfa787b
qomsad <149834513+qomsad@users.noreply.github.com>
2024-05-26 01:15:16 +0500

    исправил периодическую 502 ошибку

--- PbkService/Program.cs
+++ PbkService/Program.cs
@@ -1,4 +1,8 @@
-using static Microsoft.Extensions.Hosting.Host;
+using Microsoft.EntityFrameworkCore;
+using PbkService.Config;
+using PbkService.Data;
+using PbkService.Repositories;
+using PbkService.Services;
 
 namespace PbkService
 {
@@ -6,10 +10,54 @@ namespace PbkService
     {
         public static void Main(string[] args)
         {
-            CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>())
-                .Build()
-                .Run();
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.ConfigureCors();
+            builder.Services.ConfigureAuthentication();
+            builder.Services.ConfigureSwagger();
+
+            builder.Services.AddControllers();
+            builder.Services.AddAuthentication();
+
+            builder.Services.AddDbContext<PbkContext>(options =>
+            {
+                options.UseLazyLoadingProxies().UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            builder.Services.AddScoped<AccountService>();
+            builder.Services.AddScoped<UserRepository>();
+            builder.Services.AddScoped<MccService>();
+            builder.Services.AddScoped<MccRepository>();
+            builder.Services.AddScoped<BankRepository>();
+            builder.Services.AddScoped<BankService>();
+            builder.Services.AddScoped<TypeCardRepository>();
+            builder.Services.AddScoped<TypeCardService>();
+            builder.Services.AddScoped<ShopRepository>();
+            builder.Services.AddScoped<ShopService>();
+            builder.Services.AddScoped<OutletRepository>();
+            builder.Services.AddScoped<OutletService>();
+            builder.Services.AddScoped<PbkCategoryRepository>();
+            builder.Services.AddScoped<PbkCategoryService>();
+            builder.Services.AddScoped<MccPbkCategoryRepository>();
+            builder.Services.AddScoped<CashbackRepository>();
+            builder.Services.AddScoped<CardRepository>();
+            builder.Services.AddScoped<CardService>();
+            builder.Services.AddScoped<OperationService>();
+            builder.Services.AddScoped<OperationRepository>();
+            builder.Services.AddScoped<AlgorithmService>();
+            builder.Services.AddScoped<UserCardService>();
+            builder.Services.AddScoped<UserCardRepository>();
+
+            var app = builder.Build();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseCors("Development");
+            app.MapControllers();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.Run();
         }
     }
 }
